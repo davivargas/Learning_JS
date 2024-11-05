64679458df4391c75261a7796c866d36c6f5ca0e
Davi Vargas <davi.vargas7@gmail.com>
2024-11-05 00:11:36 -0800

    commit after challenge js#4

--- js/index.js
+++ js/index.js
@@ -1,10 +1,25 @@
-function helloWorld() {
-    console.log("Hello World!");
-    alert("hello world");
-}
+// challenge js#1
+// function helloWorld() {
+//     console.log("Hello World!");
+//     alert("hello world");
+// }
 
-function promptUserName() {
-    let userName = prompt("Enter your name: ");
-    console.log(`Your name is ${userName}`);
-    document.getElementById("userName").innerHTML = userName;
+// challenge js#2
+// function promptUserName() {
+//     let userName = prompt("Enter your name: ");
+//     console.log(`Your name is ${userName}`);
+//     document.getElementById("userName").innerHTML = userName;
+// }
+
+function confirmation() {
+    let userName = document.getElementById("answer").value;
+    console.log(userName);
+    let text = "Your input is " + userName + "\nOK or cancel";
+    if (confirm(text) == true) {
+        text = "You pressed OK!";
+        document.getElementById("userName").innerHTML = userName;
+    } else {
+        text = "You canceled!";
+    }
+    document.getElementById("messageUpdate").innerHTML = text;
 }
