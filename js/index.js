c091155269c66d2e8e90a38e1e6262bc3d9919c9
Davi Vargas <davi.vargas7@gmail.com>
2024-11-09 14:02:34 -0800

    my own version now

--- js/index.js
+++ js/index.js
@@ -33,38 +33,35 @@ function formSubmission() {
   console.log(studentCampus);
 }
 
+// challenge js#5
 function processForm() {
   document.getElementById("submit").addEventListener("click", () => {
-    //Get the name field
-    let name = document.getElementById("name").value;
-
-    //Get the set information from dropdown selection
-    let set = document.getElementById("set").value;
-
-    //Get the radio button that was pressed
-    let location = "n/a";
-    if (document.getElementById("bby-campus").checked) {
-      location = document.getElementById("bby-campus").value;
-    } else location = document.getElementById("dtc-campus").value;
-
-    //Get the boxes that were checked
-    let message = "";
-    if (document.getElementById("bus").checked)
-      message += document.getElementById("bus").value + " "; //"Bus" string
-    if (document.getElementById("car").checked)
-      message += document.getElementById("car").value + " "; //"Car" string
-    if (document.getElementById("walk").checked)
-      message += document.getElementById("walk").value + " "; //"Walk" string
-    if (document.getElementById("bike").checked)
-      message += document.getElementById("bike").value; //"Bike" string//Get the boxes that were checked
-
-    //Display the information
-    document.getElementById("name-display").innerHTML = "Your name: " + name;
-    document.getElementById("set-display").innerHTML = "Your set: " + set;
-    document.getElementById("campus-display").innerHTML =
-      "Your campus: " + location;
-    document.getElementById("transportation-display").innerHTML =
-      "Your transportation: " + message;
+    let resultsDiv = document.getElementById("results");
+    if (resultsDiv.firstChild.nodeName !== "H2") {
+      let header2 = document.createElement("h2");
+      header2.innerText = "Student data";
+      resultsDiv.insertBefore(header2, resultsDiv.firstChild);
+    }
+    let studentName = document.getElementById("name").value;
+    document.getElementById("name-display").innerHTML = "Name: " + studentName;
+    let studentSet = document.getElementById("set").value;
+    document.getElementById("set-display").innerHTML = "Set: " + studentSet;
+    let campusOptions = document.getElementsByName("campus");
+    for (i = 0; i < campusOptions.length; i++) {
+      if (campusOptions[i].checked) {
+        document.getElementById("campus-display").innerHTML =
+          "Campus: " + campusOptions[i].value;
+      }
+    }
+    let transportationText = "Transportation:";
+    let transportationOptions = document.getElementsByName("transportation");
+    for (i = 0; i < transportationOptions.length; i++) {
+      if (transportationOptions[i].checked) {
+        transportationText += " " + transportationOptions[i].value;
+      }
+      document.getElementById("transportation-display").innerHTML =
+        transportationText;
+    }
   });
 }
 processForm();
